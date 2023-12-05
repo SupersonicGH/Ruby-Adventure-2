52cdcc0061921859311979eae4a0486dddf88c28
SupersonicGH <jdford1999@yahoo.com>
2023-12-05 17:43:48 -0500

    Added code comments

--- Ruby-s-Adventure/Ruby's Adventure/Assets/Scripts/SpeedCollectible.cs
+++ Ruby-s-Adventure/Ruby's Adventure/Assets/Scripts/SpeedCollectible.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Created by Justin
 public class SpeedCollectible : MonoBehaviour
 {
     public ParticleSystem speedEffect;
